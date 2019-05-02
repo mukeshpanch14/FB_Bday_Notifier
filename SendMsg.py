b69812c549acdd2e46aa5a1f661185be24be133e
panch mukesh <mukesh.panch14@gmail.com>
2019-05-02 16:41:51 +0530

    1

--- SendMsg.py
+++ SendMsg.py
@@ -6,14 +6,14 @@ class SendMsg(object):
         self.mobileno=mobileno
     
     def SendMessage(self):
-        account_sid = "AC771e278df6a3ab92af76aaceab0855f1"
-        auth_token = "8968237e4ba957f9ef3d7819f6073afe"
+        account_sid = "account_sid"
+        auth_token = "auth_token"
         
         client = Client(account_sid, auth_token)
         
         client.api.account.messages.create(
             to=self.mobileno,
-            from_="+12062036452",
+            from_="+Twillio Number",
             body=self.message+"-panch")
             
             
