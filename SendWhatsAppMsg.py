08af76a8ed8d0d8affa11b3b05de88503013a486
panch mukesh <mukesh.panch14@gmail.com>
2019-05-02 16:41:03 +0530

    sensitive data removal

--- SendWhatsAppMsg.py
+++ SendWhatsAppMsg.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client 
  
-# account_sid = 'AC771e278df6a3ab92af76aaceab0855f1' 
-# auth_token = '8968237e4ba957f9ef3d7819f6073afe' 
+# account_sid = 'account_sid' 
+# auth_token = 'auth_token' 
 # client = Client(account_sid, auth_token) 
  
 # message = client.messages.create( 
-#                               from_='whatsapp:+14155238886',  
+#                               from_='whatsapp:+Twillio Number',  
 #                               body='Your appointment is coming up on {{July 21}} at {{3PM}}',      
 #                               to='whatsapp:+918280041455' 
 #                           ) 
@@ -21,20 +21,20 @@ class SendWhatsAppMsg(object):
         self.mobileno=mobileno
     
     def SendMessage(self):
-        account_sid = "AC771e278df6a3ab92af76aaceab0855f1"
-        auth_token = "8968237e4ba957f9ef3d7819f6073afe"
+        account_sid = "account_sid"
+        auth_token = "auth_token"
         print("Suceess")
         client = Client(account_sid, auth_token)
         
         if self.message=='No Birthdays Today':
         	client.messages.create( 
-                              from_='whatsapp:+14155238886',  
+                              from_='whatsapp:+Twillio Number',  
                               body=self.message,      
                               to='whatsapp:'+self.mobileno
                           ) 
         else:
         	client.messages.create( 
-                              from_='whatsapp:+14155238886',  
+                              from_='whatsapp:+Twillio Number',  
                               body=self.message+'\n'+'\n'+'_Call Them Immediately_',      
                               to='whatsapp:'+self.mobileno
                           ) 
